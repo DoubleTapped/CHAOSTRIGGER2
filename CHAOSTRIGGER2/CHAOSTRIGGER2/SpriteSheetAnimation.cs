3030691636a3ecdd0082328192fe7bae71409399
LeoTheGamingCat <LeoTheGamingCat@Gmail.com>
2016-04-20 08:56:21 -0400

    Started Sprite Animation

---
+++ CHAOSTRIGGER2/CHAOSTRIGGER2/SpriteSheetAnimation.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace CHAOSTRIGGER2
+{
+    public class SpriteSheetAnimation : Animation
+    {
+
+        int frameCounter;
+        int switchFrame;
+
+        Vector2 frames;
+
+        public override void LoadContent(ContentManager Content, Texture2D image, string text, Vector2 position)
+        {
+            base.LoadContent(Content, image, text, position);
+            frameCounter = 0;
+            switchFrame = 100;
+            
+        }
+
+        public override void UnloadContent()
+        {
+            base.UnloadContent();
+        }
+        public override void Update(GameTime gameTime)
+        {
+            if(isActive)
+            {
+                frameCounter += (int)gameTime.ElapsedGameTime.TotalMilliseconds;
+                if(frameCounter >= switchFrame)
+                {
+                    frameCounter = 0;
+                }
+            }
+            else
+            {
+                frameCounter = 0;
+            }
+        }
+
+        public override void Draw(SpriteBatch spriteBatch)
+        {
+            base.Draw(spriteBatch);
+        }
+    }
+}
