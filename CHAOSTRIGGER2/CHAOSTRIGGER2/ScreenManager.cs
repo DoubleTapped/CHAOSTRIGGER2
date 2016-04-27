89550d973587e640c39a934b6e25a1dd62da06e6
BrenGamer <hanson_brendan@yahoo.com>
2016-04-27 09:59:55 -0400

    AngryAtAaron

--- CHAOSTRIGGER2/CHAOSTRIGGER2/ScreenManager.cs
+++ CHAOSTRIGGER2/CHAOSTRIGGER2/ScreenManager.cs
@@ -102,7 +102,7 @@ namespace CHAOSTRIGGER2
         {
             content = new ContentManager(Content.ServiceProvider, "Content");
             currentScreen.LoadContent(Content, inputManager);
-            TitleImage = content.Load<Texture2D>("Title");
+            TitleImage = content.Load<Texture2D>("SplashScreen/Title");
             fadeTexture = content.Load<Texture2D>("FadeTest");
             fade.LoadContent(content, fadeTexture, "", Vector2.Zero);
             fade.Scale = dimensions.X;
