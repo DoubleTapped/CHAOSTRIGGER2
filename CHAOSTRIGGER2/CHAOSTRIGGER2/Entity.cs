56903a23292640e41ebe9f28d1b9d44f52b0154b
LeoTheGamingCat <LeoTheGamingCat@Gmail.com>
2016-04-28 08:24:41 -0400

    The Entity Framework 22

---
+++ CHAOSTRIGGER2/CHAOSTRIGGER2/Entity.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace CHAOSTRIGGER2
+{
+    public class Entity
+    {
+        protected int health;
+        protected Animation moveAnimation;
+        protected float moveSpeed;
+
+        protected ContentManager content;
+        protected FileManager fileManager;
+
+         public virtual void LoadContent (ContentManager content, InputManager input)
+        {
+            this.content = new ContentManager(content.ServiceProvider, "Content");
+
+        }
+
+        public virtual void UnloadContent ()
+        {
+            content.Unload();
+
+        }
+        public virtual void Update(GameTime gameTime)
+        {
+
+        }
+        public virtual void Draw(SpriteBatch spriteBatch)
+        {
+
+        }
+    }
+}
