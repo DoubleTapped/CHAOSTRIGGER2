55325fd2e6d21185ac71880e5a2302d12871f998
DoubleTapped <cpohl@students.ighamisd.org>
2016-04-28 09:22:12 -0400

    Salt & Peppa

--- CHAOSTRIGGER2/CHAOSTRIGGER2/GameplayScreen.cs
+++ CHAOSTRIGGER2/CHAOSTRIGGER2/GameplayScreen.cs
@@ -13,24 +13,28 @@ namespace CHAOSTRIGGER2
 {
     public class GameplayScreen : GameScreen
     {
+        Player player;
         public virtual void LoadContent(ContentManager content, InputManager input)
         {
             this.content = new ContentManager(content.ServiceProvider, "Content");
-
+            player = new Player();
+            player.LoadContent(content, input);
         }
 
         public virtual void UnloadContent()
         {
             content.Unload();
-
+            player.UnloadContent();
         }
         public virtual void Update(GameTime gameTime)
         {
-
+            base.Update(gameTime);
+            player.Update(gameTime);
         }
         public virtual void Draw(SpriteBatch spriteBatch)
         {
-
+            base.Draw(spriteBatch);
+            player.Draw(spriteBatch);
         }
     }
 }
